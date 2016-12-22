3ec92cccf8d9dc7c635e1d0db19ab8a7540061b1
PetroYatsiv <yatsiv01@gmail.com>
2016-12-22 16:38:08 +0200

    перекладач

---
+++ YaAPITranslator/MainWindow.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace YaAPITranslator
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_text = new System.Windows.Forms.TextBox();
+            this.output_text = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input_text
+            // 
+            this.input_text.Location = new System.Drawing.Point(12, 11);
+            this.input_text.Multiline = true;
+            this.input_text.Name = "input_text";
+            this.input_text.Size = new System.Drawing.Size(240, 134);
+            this.input_text.TabIndex = 0;
+            // 
+            // output_text
+            // 
+            this.output_text.BackColor = System.Drawing.SystemColors.Window;
+            this.output_text.Location = new System.Drawing.Point(396, 11);
+            this.output_text.Multiline = true;
+            this.output_text.Name = "output_text";
+            this.output_text.ReadOnly = true;
+            this.output_text.Size = new System.Drawing.Size(240, 134);
+            this.output_text.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 158);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.output_text);
+            this.Controls.Add(this.input_text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainWindow";
+            this.Text = "Yandex API translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_text;
+        private System.Windows.Forms.TextBox output_text;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ YaAPITranslator/MainWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+
+namespace YaAPITranslator
+{
+    public partial class MainWindow : Form
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string text = input_text.Text;
+            string key = "trnsl.1.1.20160618T185757Z.4ae7f57c17c0ab51.32931f3ae5bf01cb5d337d52b7c6496403858eb4";
+            string lang = "uk-en";
+
+            string url = String.Format(
+                "https://translate.yandex.net/api/v1.5/tr.json/translate?lang={0}&key={1}&text={2}",
+                lang, key, text    
+            );
+
+
+            WebRequest req = WebRequest.Create(url);
+            req.Method = "GET";
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            Stream dataStream = res.GetResponseStream();
+            string data = "";
+
+            using(StreamReader sr = new StreamReader(dataStream))
+            {
+                data = sr.ReadToEnd();
+                sr.Close();
+            }
+
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            data = js.Deserialize<TranslateResponse>(data).text[0];
+
+            output_text.Text = data;
+        }
+    }
+
+    struct TranslateResponse
+    {
+        public string lang;
+        public List<string> text;
+        public int code;
+    }
+}
